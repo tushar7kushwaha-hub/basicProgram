6075f366c2c731169903ddf4e74d166d0e7ff96e
tushar7kushwaha-hub <tushar7kushwaha@gmail.com>
2026-03-11 14:52:16 +0530

    Add files via upload

--- for_triangle.java
+++ for_triangle.java
@@ -1,17 +1,12 @@
 public class for_triangle {
     public static void main(String[] args) {
         for(int i=1;i<=5;i++){
-            for(int j=i;j<5;j++){
-                System.out.print(" ");
 
-            }
-            
-            for(int k=1;k<=(2*i-1);k++){
+            for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
             
-            
-            System.out.println();
+            System.out.println();    
         }
         
     }
